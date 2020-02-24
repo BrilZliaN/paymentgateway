83914cde3fafa8c2106b938d5db2a92a9781315d
BrilZliaN <brilzlian@ya.ru>
2020-02-25 00:06:37 +0300

    Добавление классов для реализации API и пример операции (не закончен)

---
+++ src/main/java/ru/ifmo/practice/gateway/controller/TransactionController.java
@@ -0,0 +1,25 @@
+package ru.ifmo.practice.gateway.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import ru.ifmo.practice.gateway.api.TransactionApiDelegate;
+import ru.ifmo.practice.gateway.api.models.TransactionStatusView;
+import ru.ifmo.practice.gateway.logic.bank.UpdateTransactionStatusOperation;
+
+@Controller
+@RequiredArgsConstructor
+public class TransactionController implements TransactionApiDelegate {
+
+    private final UpdateTransactionStatusOperation updateTransactionStatusOperation;
+
+    public ResponseEntity<Void> updateTransactionStatus(Long transactionId, TransactionStatusView transactionStatus) {
+        if (updateTransactionStatusOperation.process(transactionId, transactionStatus)) {
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_GATEWAY).build();
+        }
+    }
+
+}
