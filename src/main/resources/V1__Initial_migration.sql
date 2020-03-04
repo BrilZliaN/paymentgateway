ded1486484c01215994b94c8d5c5810f5f9d19ff
DiplomateProgrammer <34778173+DiplomateProgrammer@users.noreply.github.com>
2020-03-04 19:05:25 +0300

    Add files via upload

--- src/main/resources/V1__Initial_migration.sql
+++ src/main/resources/V1__Initial_migration.sql
@@ -1,27 +1,28 @@
 CREATE TABLE card (
-    id INT NOT NULL,
+    id BIGINT NOT NULL,
     PRIMARY KEY (id),
-    date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     number VARCHAR(25),
     valid VARCHAR(5),
 	owner VARCHAR(100)
 );
 CREATE TABLE invoice (
-    id INT NOT NULL,
+    id BIGINT NOT NULL,
     PRIMARY KEY (id),
-    date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     sum DOUBLE
 );
 
 CREATE TABLE transaction (
-    id INT NOT NULL,
+    id BIGINT NOT NULL,
     PRIMARY KEY (id),
-    date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     status_code INT,
-    FK_card_id INT,
-    FK_invoice_id INT,
-    CONSTRAINT FK_card_id FOREIGN KEY (id)
+    status_date TIMESTAMP,
+    card_id INT,
+    invoice_id INT,
+    CONSTRAINT card_id FOREIGN KEY (id)
     REFERENCES card(id),
-    CONSTRAINT FK_invoice_id FOREIGN KEY (id)
+    CONSTRAINT invoice_id FOREIGN KEY (id)
     REFERENCES invoice(id)
 );
