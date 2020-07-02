bd942f3b53e0b8bb203bc2293acdf479cac52e26
BrilZliaN <brilzlian@ya.ru>
2020-07-02 23:51:15 +0300

    #15 Make /transaction endpoints secure

---
+++ src/main/java/ru/ifmo/practice/gateway/configuration/SecurityConfiguration.java
@@ -0,0 +1,53 @@
+package ru.ifmo.practice.gateway.configuration;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import ru.ifmo.practice.gateway.security.AuthenticationEntryPoint;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private static final String ACQUIRER_ROLE = "USER";
+
+    private final AuthenticationEntryPoint authenticationEntryPoint;
+
+    @Value("${security.acquirer.username}")
+    private String acquirerUsername;
+
+    @Value("${security.acquirer.password}")
+    private String acquirerPassword;
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser(acquirerUsername).password(passwordEncoder().encode(acquirerPassword))
+                .authorities(ACQUIRER_ROLE);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().cors().disable()
+                .authorizeRequests()
+                .antMatchers("/transaction").authenticated()
+                .anyRequest().permitAll()
+                .and()
+                .httpBasic()
+                .authenticationEntryPoint(authenticationEntryPoint);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ src/main/java/ru/ifmo/practice/gateway/security/AuthenticationEntryPoint.java
@@ -0,0 +1,25 @@
+package ru.ifmo.practice.gateway.security;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class AuthenticationEntryPoint extends BasicAuthenticationEntryPoint {
+
+	@Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authEx) {
+        response.addHeader("WWW-Authenticate", "Basic realm=" +getRealmName());
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+    }
+
+	@Override
+    public void afterPropertiesSet() {
+        setRealmName("");
+        super.afterPropertiesSet();
+    }
+
+}
