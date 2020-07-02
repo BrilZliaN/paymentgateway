968130d7ae39e190cc8b6b7bac9452ab5327e135
BrilZliaN <brilzlian@ya.ru>
2020-07-02 23:07:07 +0300

    WIP beautify menu

--- src/main/java/ru/ifmo/practice/gateway/controller/ui/MainView.java
+++ src/main/java/ru/ifmo/practice/gateway/controller/ui/MainView.java
@@ -1,7 +1,6 @@
 package ru.ifmo.practice.gateway.controller.ui;
 
-import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.menubar.MenuBar;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.router.Route;
@@ -10,15 +9,15 @@ import com.vaadin.flow.router.RouterLink;
 
 @Push
 @Route("")
-public class MainView extends Div implements RouterLayout {
+public class MainView extends VerticalLayout implements RouterLayout {
 
     public MainView() {
-        var layout = new VerticalLayout();
-        var label = new Label("payment-gateway");
-        var allTransactionsLink = new RouterLink("View all transactions", AllTransactionsView.class);
-        var openPaymentForm = new RouterLink("Open payment form", PaymentFormView.class);
-        var openPaymentStatus = new RouterLink("Open payment status", PaymentStatusView.class);
-        layout.add(label, allTransactionsLink, openPaymentForm, openPaymentStatus);
-        add(layout);
+        var bar = new MenuBar();
+        bar.addItem(new RouterLink("Payment gateway", MainView.class));
+        bar.addItem(new RouterLink("Payment form", PaymentFormView.class));
+        bar.addItem(new RouterLink("Payment status", PaymentStatusView.class));
+        bar.addItem(new RouterLink("All transactions", AllTransactionsView.class));
+        add(bar);
+        setHorizontalComponentAlignment(Alignment.CENTER, bar);
     }
 }
